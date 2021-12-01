320c7a7da0c5d0ead33ecf693900c7da68ab6d36
unknown <tamilselvibca97@gmail.com>
2021-12-01 19:28:07 +0530

    first commit

---
+++ wish.js
@@ -0,0 +1,78 @@
+function gm()
+{
+    var h2Ref=document.querySelector("h2");
+    console.log(h2Ref);
+    h2Ref.innerText="GOOD MORNING!!!";
+    h2Ref.style.color="maroon";
+    document.getElementById("p1").innerHTML = "Good morning to you. May every step you make be filled with happiness, love, and peace!";
+}
+function ga()
+{
+    var h2Ref=document.querySelector("h2");
+    console.log(h2Ref);
+    h2Ref.innerText="GOOD AFTERNOON"; 
+    h2Ref.style.color="hotpink";
+
+    document.getElementById("p1").innerHTML = "You are the best man in this world. In this sweet afternoon, I want to tell you that you have made my world beautiful. Good afternoon. Enjoy your lunch!";
+
+}
+function ge()
+{
+    var h2Ref=document.querySelector("h2");
+    console.log(h2Ref);
+    h2Ref.innerText="GOOD EVENING!!!";
+    h2Ref.style.color="white";
+
+    document.getElementById("p1").innerHTML = "Evening becomes special without a company and you are my best companion, my love. Evenings, where I get to spend my time with you, are pure blessings!";
+
+}
+function gn()
+{
+    var h2Ref=document.querySelector("h2");
+    console.log(h2Ref);
+    h2Ref.innerText="GOOD NIGHT!!!";
+    h2Ref.style.color="black";
+
+    document.getElementById("p1").innerHTML = "Have a peaceful night and leave all your tiredness behind. May He watch over you and protect you.";
+
+}
+function birth()
+{
+    var h2Ref=document.querySelector("h2");
+    console.log(h2Ref);
+    h2Ref.innerText="HAPPY BIRTHDAY!!!";
+    h2Ref.style.color="violet";
+
+    document.getElementById("p1").innerHTML = "Our age is merely the number of years the world has been enjoying us!";
+
+}
+function valan()
+{
+    var h2Ref=document.querySelector("h2");
+    console.log(h2Ref);
+    h2Ref.innerText="HAPPY VALANTINES";
+    h2Ref.style.color="red";
+
+    document.getElementById("p1").innerHTML = "You are my sunshine, sweet blessing and drop of joy. I wouldn’t have been able to do anything without you. I love you!";
+
+}
+function anni()
+{
+    var h2Ref=document.querySelector("h2");
+    console.log(h2Ref);
+    h2Ref.innerText="HAPPY WEDDING ANNIVERSARY!!!";
+    h2Ref.style.color="skyblue";
+
+    document.getElementById("p1").innerHTML = "Happy Wedding Anniversary, my love. How lucky I am to have you in my life!";
+
+}
+function cong()
+{
+    var h2Ref=document.querySelector("h2");
+    console.log(h2Ref);
+    h2Ref.innerText="CONGRATULATIONS!!!";
+    h2Ref.style.color="orangered";
+
+    document.getElementById("p1").innerHTML = "Congratulations on your new job! I am so happy to see you fulfilling all your dreams. You deserve this job, and I am so proud of you— best of luck for your bright future!";
+
+}
